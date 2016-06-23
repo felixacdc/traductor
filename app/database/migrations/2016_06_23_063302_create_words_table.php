e228184ec280b2ea9b3a0e1f0db965d0b21e14b2
felixacdc <pfmata01@gmail.com>
2016-06-23 00:40:43 -0600

    [add] translate to spanish and create database tables

---
+++ app/database/migrations/2016_06_23_063302_create_words_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateWordsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('words', function (Blueprint $table) {
+            $table->increments('id');
+            $table->longText('spanish');
+            $table->longText('english');
+            $table->integer('idCategory')->unsigned();
+
+            $table->foreign('idCategory')
+                    ->references('id')
+                    ->on('categories');
+                    
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('words');
+    }
+}
