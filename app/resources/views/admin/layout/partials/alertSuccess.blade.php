6bb6ccc2e2e9eb3c766fe8a91dc8cb2109567335
felixacdc <pfmata01@gmail.com>
2016-07-08 02:35:26 -0600

    [modify] alerts

--- app/resources/views/admin/layout/partials/alertSuccess.blade.php
+++ app/resources/views/admin/layout/partials/alertSuccess.blade.php
@@ -1,8 +1,8 @@
 @if( Session::has('message') )
     <div class="row">
-        <div class="col-md-4"></div>
-        <div class="col-md-4">
-            <div class="alert alert-success homeloader wow animated fadeInDown" id="error">
+        <div class="col-md-3"></div>
+        <div class="col-md-6">
+            <div class="alert alert-success alert-block homeloader wow animated fadeInDown">
                 <b>Alerta!</b> {{Session::get('message')}}
             </div>
         </div>
